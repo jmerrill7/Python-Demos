f4af8df9afbc3948ed18fb26446420f00e515862
James Merrill <jamesmerrill7@gmail.com>
2017-02-28 15:34:54 -0800

    simplified sorting algorithm

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -12,12 +12,5 @@ def bubbleSort(myList):
 myList = [67,45,2,13,1,998]
 print(bubbleSort(myList))
 
-def bubbleSort2(myList2):
-    for x in range(0, len(myList2) -1):
-        for y in range(0, len(myList2) -1 - x):
-            if myList2[y] > myList2[y+1]:
-                myList2[y], myList2[y+1] = myList2[y+1], myList2[y]
-    return myList2
-
 myList2 = [89,23,33,45,10,12,45,45,45]
-print(bubbleSort2(myList2))
+print(bubbleSort(myList2))
